0aeacd1d37b2c28ab61a61563d73773f13db745d
Unknown <stcyliax@gmail.com>
2017-07-14 23:05:58 +0200

    Finished lab

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -1,6 +1,7 @@
 import pickle
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from alexnet import AlexNet
 
 # TODO: Load traffic signs data.
@@ -27,6 +28,7 @@ fc7 = AlexNet(resized, feature_extract=True)
 fc7 = tf.stop_gradient(fc7)
 
 # TODO: Add the final layer for traffic sign classification.
+nb_classes = 43
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
 fc8W = tf.Variable(tf.truncated_normal(shape, stddev=1e-2))
 fc8b = tf.Variable(tf.zeros(shape[1]))
@@ -38,9 +40,9 @@ probs = tf.nn.softmax(logits)
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
 y = tf.placeholder(tf.int32, (None))
-one_hot_y = tf.one_hot(y, n_classes)
+one_hot_y = tf.one_hot(y, nb_classes)
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=probs)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
 training_operation = optimizer.minimize(loss_operation)
@@ -49,3 +51,34 @@ correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # TODO: Train and evaluate the feature extraction model.
+
+BATCH_SIZE = 128
+EPOCHS = 10
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+
+        validation_accuracy = evaluate(X_valid, y_valid)
+        print("EPOCH {} ...".format(i + 1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
